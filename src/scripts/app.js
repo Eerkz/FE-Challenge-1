5955619cb4c27b1bac5ab3099b2a9d7f1a4fcce6
Eerkz <wcalemios@gmail.com>
2022-01-11 21:40:19 +0800

    showed info from json file, added click card and show info on header effect

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,11 +2,14 @@
 //get elements
 const container = document.querySelector('.info-cont');
 const headerTitle = document.querySelectorAll('.header-title');
+const headerAuthor = document.querySelector('.header-author');
+const headerDate = document.querySelector('.header-date');
 const img = document.querySelectorAll('img');
 const cardTitle = document.querySelectorAll('.card-title');
 const description = document.querySelectorAll('.description');
 const author = document.querySelectorAll('.author');
 const date = document.querySelectorAll('.date');
+const cards = document.querySelectorAll('.card')
 
 //fetch local json data
 fetch("/src/scripts/data.json")
@@ -18,12 +21,25 @@ fetch("/src/scripts/data.json")
     });
 
 const displayData = (data) => {
-    data.forEach(data => {
-        // console.log(data)
-        showUI(data)
-    })
+    let dateFormat = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let postedDate = new Date(data.date)
+
+    for (let i=0; i < data.length; i++){
+        author[i].innerText = data[i].author
+        cardTitle[i].innerText = data[i].title
+        description[i].innerText = data[i].description
+    }
+
+    for (let i = 0; i < cards.length; i++){
+        cards[i].addEventListener('click', ()=> {
+            showHeader(i,data)
+        })
+    }
 }
 
-const showUI = (data) => {
-    console.log(data)
+const showHeader = (cardNum,data) => {
+    let clickedData = data[cardNum]
+    headerTitle[0].innerText = clickedData.title
+    headerAuthor.innerText = clickedData.author
+    // headerDate[0].innerText = clickedData.author
 }
